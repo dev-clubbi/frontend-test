30363e5cfed3e22b98d5f689c6b5ddefeb7b258b
vanessabastos <vanessarsb83@gmail.com>
2022-10-04 17:39:22 -0300

    Implementação de inputs e buttons

--- clubbi/src/App.js
+++ clubbi/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Actor from './Components/Actor';
+import Films from './Components/Films';
+import Location from './Components/Location';
 
 export default class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
   render() {
     return (
-      <div>App</div>
+      <div>
+        <h1>Clubbi</h1>
+        <section>
+          <Actor />
+          <Films />
+          <Location />
+        </section>
+      </div>
     )
   }
 }
